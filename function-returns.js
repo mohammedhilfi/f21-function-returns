8507ee0127831330b4a4e5edfa0e092297bef231
Jonathan C <jsea@uw.edu>
2021-09-23 18:54:35 -0700

    Clarified some instructions

--- function-returns.js
+++ function-returns.js
@@ -4,13 +4,15 @@ function ConvertDegreesFToC(degreesF)
 
 	// 2) We need to give back the Celsius degrees to whomever
 	//      invoked the function, not just calculate them
+	//      (i.e. use a return keyword)
 }
 
 let temperatureInF = 76;
 let temperatureInC;
 
-// 1) We need to invoke our conversion function and
-//      store the given-back degrees into "temperatureInC";
+// 1) We need to invoke our conversion function with the
+//      76-degrees Fahrenheit variable and store the
+//      given-back degrees into "temperatureInC";
 //      so that "console.log" prints correctly
 
 console.log(temperatureInF + " degrees Fahrenheit equates to " + temperatureInC + " degrees in Celsius.");
